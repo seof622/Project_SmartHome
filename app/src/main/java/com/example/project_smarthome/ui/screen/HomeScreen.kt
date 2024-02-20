b1066f48022cc6d5d2403e9abfdb9c82c479f499
TaeHyun An <seof622@naver.com>
2024-02-21 01:59:19 +0900

    Feat: Change UI

--- app/src/main/java/com/example/project_smarthome/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/project_smarthome/ui/screen/HomeScreen.kt
@@ -1,17 +1,24 @@
 package com.example.project_smarthome.ui.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Tv
+import androidx.compose.material.icons.outlined.PowerSettingsNew
+import androidx.compose.material.icons.outlined.Tv
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
@@ -24,11 +31,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.project_smarthome.R
 import com.example.project_smarthome.data.mockDeviceList
 import com.example.project_smarthome.network.Device
 import com.example.project_smarthome.network.DeviceStatus
@@ -55,39 +65,47 @@ fun DeviceStatusCard(modifier: Modifier = Modifier, device: Device) {
         colors = CardDefaults.cardColors(containerColor = cardColor)
     ){
         Column (
-            modifier = Modifier.padding(12.dp),
+            modifier = Modifier.padding(20.dp),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Row(horizontalArrangement = Arrangement.SpaceBetween) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 Icon(
-                    painter = ,
+                    Icons.Outlined.Tv,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .align(Alignment.CenterVertically),
                     contentDescription = null
                 )
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        painter = ,
-                        contentDescription = null
-                    )
-                }
+                Icon(
+                    Icons.Outlined.PowerSettingsNew,
+                    modifier = Modifier
+                        .clickable {  },
+                    contentDescription = null
+                )
             }
-            Text(
-                text = device.category,
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Text(
-                text = device.location,
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Text(
-                text = if (device.status == DeviceStatus.ON) {
-                    "켜짐"
-                } else if (device.status == DeviceStatus.OFF){
-                    "꺼짐"
-                } else {
-                    "로딩 중"
-                },
-                style = MaterialTheme.typography.bodyMedium
-            )
+            Column {
+                Text(
+                    text = device.location,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = device.category,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = if (device.status == DeviceStatus.ON) {
+                        "켜짐"
+                    } else if (device.status == DeviceStatus.OFF){
+                        "꺼짐"
+                    } else {
+                        "로딩 중"
+                    },
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
@@ -103,7 +121,7 @@ fun DeviceGridScreen(modifier: Modifier = Modifier, devices: List<Device>) {
     ) {
         items(items = devices, key = {device -> device.id}) {device->
             DeviceStatusCard(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxSize(),
                 device = device
             )
         }
@@ -116,7 +134,7 @@ fun SmartHomeAppBar(modifier : Modifier = Modifier) {
     CenterAlignedTopAppBar(
         title = {
             Text(
-                text = "우리 집 ",
+                text = stringResource(R.string.HomeScreenTitle),
                 fontSize = 20.sp
             )
         }
