72585116b9ac0b7e003feeffc0cb37ef32da8170
ath <seof622@naver.com>
2024-12-09 10:50:32 +0900

    feat: add Mqtt Repository

---
+++ app/src/main/java/com/example/SmartHome/data/Network/MQTTRepository.kt
@@ -0,0 +1,45 @@
+package com.example.SmartHome.data.Network
+
+import android.util.Log
+import com.example.SmartHome.data.mqttIP
+import com.example.SmartHome.data.mqttPort
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttMessage
+
+class MQTTRepository {
+    private val mqttURL: String = "tcp://$mqttIP:$mqttPort"
+
+    private val mqttClient = MqttClient(mqttURL, MqttClient.generateClientId(), null).apply {
+        this.setCallback(object : MqttCallback {
+            override fun connectionLost(cause: Throwable?) {
+                Log.e("MQTT", "Connection Lost")
+            }
+
+            override fun messageArrived(topic: String?, message: MqttMessage?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                Log.e("MQTT", "Message Delivery Complete")
+            }
+        })
+    }
+
+    fun connect() {
+        mqttClient.connect()
+    }
+
+    fun subscribe(topic: String) {
+        mqttClient.subscribe(topic)
+    }
+
+    fun disconnect() {
+        mqttClient.disconnect()
+    }
+
+    fun publish (topic: String, data: ByteArray) {
+        mqttClient.publish(topic, MqttMessage(data))
+    }
+}
